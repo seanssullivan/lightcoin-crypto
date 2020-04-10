d09f8a3506b141bcff2482dda8eb9a1b89448550
Sean Sullivan <dev@seanmedia.ca>
2020-04-10 19:51:43 +0000

    Add array of transactions to account class

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+
+  get balance() {
+    return this.transactions.reduce((sum, curr) => sum + curr.value);
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
@@ -14,20 +22,24 @@ class Transaction {
     this.account = account;
   }
 
+  commit() {
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
 }
 
 class Deposit extends Transaction{
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 }
@@ -39,14 +51,17 @@ class Withdrawal extends Transaction {
 const myAccount = new Account("snow-patrol");
 
 t1 = new Withdrawal(50.25, myAccount);
+console.log('Transaction Amount:' + t1.value);
 t1.commit();
 console.log('Transaction 1:', t1);
 
 t2 = new Withdrawal(9.99, myAccount);
+console.log('Transaction Amount:' + t2.value);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposite(120.00, myAccount);
+t3 = new Deposit(120.00, myAccount);
+console.log('Transaction Amount:' + t3.value);
 t3.commit();
 console.log('Transaction 3:', t3);
 

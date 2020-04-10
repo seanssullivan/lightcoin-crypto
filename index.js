1cc04aefb8ad29c79a940740c4a47b866e92cb31
Sean Sullivan <dev@seanmedia.ca>
2020-04-10 19:38:43 +0000

    Add Transaction superclass for Deposit and Withdrawal

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ class Account {
 
 }
 
+class Transaction {
 
-class Withdrawal {
-
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
+    this.account = account;
   }
 
 }
 
-class Deposit {
-
-  constructor(amount) {
-    this.amount = amount;
-  }
+class Deposit extends Transaction{
 
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  commit() {
+    this.account.balance -= this.amount;
   }
 
 }
@@ -36,16 +36,18 @@ class Deposit {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposite(120.00);
+t3 = new Deposite(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
